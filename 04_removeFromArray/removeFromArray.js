82190a8f68170213262d64d312564071597f7a65
Julian Lopez <julianmatthewlopez@gmail.com>
2022-02-22 10:47:03 -0800

    Finished exercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, ...remove) {
+    let a = arr;
+    let b = remove;
+    let c = a.filter(d => !b.includes(d));
+    return c;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
