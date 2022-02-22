bd01432fbaf2fb34d077d42693a2908d25aac0dd
Julian Lopez <julianmatthewlopez@gmail.com>
2022-02-21 16:08:43 -0800

    Finished exercise 02.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+    let repeatedString = '';
+    while (number > 0){
+        repeatedString += string; 
+        number--;
+    }
+    return repeatedString;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
